0d92fca94ccb6d5377c1b67601969b77bd028142
Catherine Edelveis <ekaterina.edelveis@gmail.com>
2024-06-14 16:57:34 +0300

    add custom validator

--- src/main/java/dev/cat/modular/monolith/customer/validation/PriceConstraintValidator.java
+++ src/main/java/dev/cat/modular/monolith/customer/validation/PriceConstraintValidator.java
@@ -1,12 +1,21 @@
 package dev.cat.modular.monolith.customer.validation;
 
+import dev.cat.modular.monolith.calculator.CalculatorAPI;
+import dev.cat.modular.monolith.dto.calculator.CalculatorRequest;
+import dev.cat.modular.monolith.dto.shipment.ShipmentRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
 
-public class PriceConstraintValidator implements ConstraintValidator<ShipmentPrice, Double> {
+@RequiredArgsConstructor
+public class PriceConstraintValidator implements ConstraintValidator<ShipmentPrice, ShipmentRequest> {
+
+    private final CalculatorAPI calculatorAPI;
 
     @Override
-    public boolean isValid(Double aDouble, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
+    public boolean isValid(ShipmentRequest request, ConstraintValidatorContext constraintValidatorContext) {
+
+        double price = calculatorAPI.calculatePrice(new CalculatorRequest(request.weight(), request.from(), request.to()));
+        return request.price() == 0.0 || request.price() == price;
     }
 }
--- src/main/java/dev/cat/modular/monolith/customer/validation/ShipmentPrice.java
+++ src/main/java/dev/cat/modular/monolith/customer/validation/ShipmentPrice.java
@@ -11,4 +11,5 @@ import java.lang.annotation.Target;
 @Target( { ElementType.METHOD, ElementType.FIELD } )
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ShipmentPrice {
+    String message() default "Shipment price doesn't match the requested price.";
 }
