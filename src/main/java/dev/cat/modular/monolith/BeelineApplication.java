158675118df8dd11db7b305bb8d43a9764352b1c
Catherine Edelveis <ekaterina.edelveis@gmail.com>
2024-06-18 17:01:24 +0300

    add enablewebmvc

--- src/main/java/dev/cat/modular/monolith/BeelineApplication.java
+++ src/main/java/dev/cat/modular/monolith/BeelineApplication.java
@@ -2,7 +2,9 @@ package dev.cat.modular.monolith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class BeelineApplication {
 
